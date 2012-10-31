e9b0d65c6279b35e86f858b765d0b94cdebf3b0e
Whizzpopper <workshop@Benjamins-MacBook-Pro.local>
2012-10-30 20:33:59 -0500

    Recording monitor adusted to screen size
    This should adjust recording screen to correct size/view

--- libcamera/SecCamera.h
+++ libcamera/SecCamera.h
@@ -82,8 +82,6 @@ namespace android {
 #error "Please define the Camera module"
 #endif
 
-#define M5MO_PREVIEW_WIDTH            1280
-#define M5MO_PREVIEW_HEIGHT           720
 #define M5MO_SNAPSHOT_WIDTH           3264
 #define M5MO_SNAPSHOT_HEIGHT          2448
 
