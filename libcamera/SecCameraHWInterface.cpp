058cfa2a9a0cf49e52e0b301fb57babe79baf7f9
Whizzpopper <ben@jikkenkobo.org>
2012-11-06 22:55:04 -0600

    Update libcamera/SecCameraHWInterface.cpp
    Correct formatting issues

--- libcamera/SecCameraHWInterface.cpp
+++ libcamera/SecCameraHWInterface.cpp
@@ -161,7 +161,8 @@ void CameraHardwareSec::initDefaultParameters(int cameraId)
     if (cameraId == SecCamera::CAMERA_ID_BACK) {
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "800x480,720x480,640x480,592x480,352x288");
-        p.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,             "3264x2448,3264x1968,2560x1920,2560x1536,2048x1536,2048x1232,1600x1200,1600x960,800x480,640x480");
+        p.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
+        "3264x2448,3264x1968,2560x1920,2560x1536,2048x1536,2048x1232,1600x1200,1600x960,800x480,640x480");
     } else {
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "640x480,352x288,320x240,176x144");
